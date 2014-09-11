df047471057b6d2fe2759b753b53db48ccac5f42
Wael Showair <show0017@algonquinlive.com>
2014-09-11 18:57:45 -0400

    toggle navigation display upon image click

--- js/ex-3.js
+++ js/ex-3.js
@@ -17,4 +17,40 @@ Make sure you uncomment the CSS file
 where it is setting the nav menu to
 display:none;
 ****************************************/
+/* Extra function to toggle value of CSS display property. Just to improve the code redability. */
+var toggleDisplay = function (displayValue){
 
+    /* style.display will only return a valid value ("none" or "block") when it’s explicitly set from JS or from the style attribute which
+       is applied to the HTML tag in HTML file. Since it is set inside CSS file, it will retrun empty string for the first time only.
+       Handle this corner case by checking if display value is empty string, just toggle it to block value. */
+    return (("none" === displayValue) ||
+             ("" === displayValue))? "block":"none";
+}
+
+/* According to second requirement, this function will run on each time, image is clicked. */
+var onImageClicked = function (){
+
+    console.log("image has been clicked");
+
+    /* Get a reference to node element that represents the image whose class is mainnav. */
+    var navElemNode = document.querySelector(".mainnav");
+
+    /* Toggle display of main navigation element between none and block values. */
+    navElemNode.style.display = toggleDisplay(navElemNode.style.display);
+    //console.log("After toggling: ", navElemNode.style.display);
+}
+
+/* According to the first requirement, this function will run after loading the page. */
+var onPageLoadingFinished = function(){
+
+    console.log("webpage has been loaded successfully");
+
+    /*  Get a reference to node element that represents the image whose class is menu-toggle. */
+    var imgElemNode = document.querySelector(".menu-toggle");
+
+    /* Set click event handler for the image node element. */
+    imgElemNode.onclick = onImageClicked;
+}
+
+/* Make sure not to access web contents after loading the webpage successfully. */
+window.onload = onPageLoadingFinished;
